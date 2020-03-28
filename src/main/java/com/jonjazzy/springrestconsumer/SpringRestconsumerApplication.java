4082b4e388f2e47f0c186cfa1871e4dbd34bb647
Jon Jazzy <jonjazzy123@gmail.com>
2020-03-28 12:42:45 -0600

    My First Commit

---
+++ src/main/java/com/jonjazzy/springrestconsumer/SpringRestconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.jonjazzy.springrestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestconsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonjazzy/springrestconsumer/SpringRestconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonjazzy.springrestconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
